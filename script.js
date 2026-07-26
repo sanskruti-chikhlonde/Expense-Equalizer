5b7363fb9f494a7667f6b0b624a17147d9ce8ee5
sanskruti-chikhlonde <sanskruti0804@gmail.com>
2026-07-26 22:43:31 +0530

    Added input handling and validation logic

---
+++ script.js
@@ -0,0 +1,44 @@
+const billInput = document.getElementById("billAmount");
+const peopleInput = document.getElementById("people");
+const tipInput = document.getElementById("tip");
+const totalBillOutput = document.getElementById("totalBill");
+const tipAmountOutput = document.getElementById("tipAmount");
+const finalBillOutput = document.getElementById("finalBill");
+const perPersonOutput = document.getElementById("perPerson");
+
+const calculateBtn = document.getElementById("calculateBtn");
+
+calculateBtn.addEventListener("click", function () {
+
+    console.log("Button Clicked");
+
+    const bill = Number(billInput.value);
+    const people = Number(peopleInput.value);
+    const tip = Number(tipInput.value);
+    
+    //validation
+
+    if (bill <=0) {
+        alert("Please enter a valid bill amount.");
+        return;
+    }
+
+    if (people <= 0) {
+        alert("Please enter a valid number of people.");
+        return;
+    }
+
+    if (tip < 0) {
+        alert("Please enter a valid tip percentage.");
+        return;
+    }
+
+    //calculation
+
+    const tipAmount = (bill * tip) / 100;
+
+    console.log(bill);
+    console.log(people);
+    console.log(tip);
+    console.log(tipAmount);
+});
